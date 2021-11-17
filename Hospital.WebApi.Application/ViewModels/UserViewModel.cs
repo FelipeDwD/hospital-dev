14859b6146515b7d846a54ed96b038780f7c5666
Felipe Arruda <felipeneves089@gmail.com>
2021-11-17 18:40:36 -0300

    GDA-16 Config model map

---
+++ Hospital.WebApi.Application/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hospital.WebApi.Application.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
