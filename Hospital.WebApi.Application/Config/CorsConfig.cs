6953d1fb9d60e0ef1a90af66a9450adf00e96de8
Felipe Arruda <felipeneves089@gmail.com>
2021-11-17 15:56:44 -0300

    GDA-15 Config Cors

---
+++ Hospital.WebApi.Application/Config/CorsConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Hospital.WebApi.Application.Config
+{
+    public static class CorsConfig
+    {
+        public static IServiceCollection GetConfigCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Development",
+                    builder =>
+                    builder
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin());
+            });     
+    }
+}
