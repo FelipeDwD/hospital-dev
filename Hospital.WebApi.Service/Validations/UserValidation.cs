bd077f2be5655f6526112d4bd4fcc607f2d9e943
Felipe Arruda <felipeneves089@gmail.com>
2021-11-17 10:11:45 -0300

    GDA-12 Add Validation for User

---
+++ Hospital.WebApi.Service/Validations/UserValidation.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Hospital.WebApi.Domain.Entities;
+using System;
+
+namespace Hospital.WebApi.Service.Validations
+{
+    public class UserValidation : AbstractValidator<UserModel>
+    {
+        public UserValidation()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("O campo Nome é obrigatório")
+                .Length(min: 3, max: 20).WithMessage("O campo Nome deve ter entre 3 a 20 caracteres");
+
+            RuleFor(x => x.Surname)
+                .NotEmpty().WithMessage("O campo Sobrenome é obrigatório")
+                .Length(min: 2, max: 20).WithMessage("O campo Nome deve ter entre 2 a 20 caracteres");
+
+            RuleFor(x => x.BirthDate)
+                .NotEmpty().WithMessage("O Campo Data de Nascimento é obrigatório")
+                .Must(IsValidYear).WithMessage("O ano de nascimetno deve ser menor que o ano atual")
+                .Must(IsValidAge).WithMessage("O colaborador deve ter no mínimo 15 anos");            
+        }
+
+        private static bool IsValidYear(DateTime currentYear) =>
+            currentYear.Year < DateTime.Now.Year;
+
+        private static bool IsValidAge(DateTime birthDate) =>
+            birthDate <= DateTime.Now.AddYears(-15);        
+    }
+}
