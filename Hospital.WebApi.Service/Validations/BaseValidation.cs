dcd81b564c2bea1ef692ce8215556d360e78d80f
Felipe Arruda <felipeneves089@gmail.com>
2021-11-17 12:17:20 -0300

    GDA-13 Developed Base Validation

---
+++ Hospital.WebApi.Service/Validations/BaseValidation.cs
@@ -0,0 +1,11 @@
+﻿namespace Hospital.WebApi.Service.Validations
+{
+    public static class BaseValidation
+    {
+        public static string RequiredField(string field) =>
+            $"O campo {field} é obrigatório.";
+
+        public static string FieldInvalidFormat(string field) =>
+            $"O campo {field} não possui um formato válido.";
+    }
+}
