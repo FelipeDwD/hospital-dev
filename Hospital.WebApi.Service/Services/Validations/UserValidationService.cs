96b0cf804260670a3a3c8148ce162316dda6e525
Felipe Arruda <felipeneves089@gmail.com>
2021-11-17 15:00:56 -0300

    GDA-14 Create services of Validation

---
+++ Hospital.WebApi.Service/Services/Validations/UserValidationService.cs
@@ -0,0 +1,15 @@
+﻿using Hospital.WebApi.Domain.Interfaces.Services.Validations;
+using Hospital.WebApi.Domain.Models;
+using Hospital.WebApi.Service.Validations;
+
+namespace Hospital.WebApi.Service.Services.Validations
+{
+    public class UserValidationService : ValidationBaseService<UserModel>, IUserValidationService
+    {
+        public UserValidationService(UserValidation userValidation)
+        : base(userValidation)
+        {
+
+        }
+    }
+}
---
+++ Hospital.WebApi.Service/Services/Validations/ValidationBaseService.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Hospital.WebApi.Domain.Interfaces.Services.Validations;
+using Hospital.WebApi.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hospital.WebApi.Service.Services.Validations
+{
+    public abstract class ValidationBaseService<T> : IValidationBaseService<T> where T : BaseEntityModel
+    {
+        private AbstractValidator<T> _typeValidation;
+
+        public ValidationBaseService(AbstractValidator<T> typeValidation) =>
+            _typeValidation = typeValidation;        
+
+        public List<string> Validate(T entity)
+        {
+            var validator = _typeValidation.Validate(entity);
+
+            if (validator.IsValid)
+                return null;
+
+            var messages = GetMessages(validator);
+            return messages;
+        }
+
+        private List<string> GetMessages(ValidationResult validationResult)
+        {
+            var errors = (from error in validationResult.Errors
+                          select error.ErrorMessage).ToList();
+
+            return errors;
+        }
+    }
+}
