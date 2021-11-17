8e202d56d87f9f0cfeac195b0457f94393e212c7
Felipe Arruda <felipeneves089@gmail.com>
2021-11-17 18:39:56 -0300

    GDA-16 Config AutoMapper

---
+++ Hospital.WebApi.Application/Config/AutoMapperProfileConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Hospital.WebApi.Application.ViewModels;
+using Hospital.WebApi.Domain.Models;
+
+namespace Hospital.WebApi.Application.Config
+{
+    public class AutoMapperProfileConfig : Profile
+    {
+        public AutoMapperProfileConfig()
+        {
+            CreateMap<UserModel, UserViewModel>().ReverseMap();
+            CreateMap<ContactModel, ContactViewModel>().ReverseMap();
+        }
+    }
+}
