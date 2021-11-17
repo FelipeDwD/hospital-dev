3a0eb929d37a6a1c34f9f0e58a5ce6ba04db680d
Felipe Arruda <felipeneves089@gmail.com>
2021-11-17 12:18:33 -0300

    GDA-13 Developed Tests To Base Validation

---
+++ Hospital.WebApi.Service.Tests/ValidationTests/BaseValidationTests/FieldInvalidFormatTest.cs
@@ -0,0 +1,20 @@
+﻿using Hospital.WebApi.Service.Validations;
+using Xunit;
+
+namespace Hospital.WebApi.Service.Tests.ValidationTests.BaseValidationTests
+{
+    public class FieldInvalidFormatTest
+    {
+        [Fact]
+        public void GivenFieldNameMustReturnFullPhrase()
+        {
+            //Arrange
+            var fieldName = "Celular";
+            var expected = "O campo Celular não possui um formato válido.";
+            //Act
+            var actual = BaseValidation.FieldInvalidFormat(fieldName);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
