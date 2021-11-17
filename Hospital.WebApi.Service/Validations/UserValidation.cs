b100df305e13c8d09c64292c8d35d400209d0ccb
Felipe Arruda <felipeneves089@gmail.com>
2021-11-17 12:17:41 -0300

    GDA-13 Alter User Validation

--- Hospital.WebApi.Service/Validations/UserValidation.cs
+++ Hospital.WebApi.Service/Validations/UserValidation.cs
@@ -9,15 +9,15 @@ namespace Hospital.WebApi.Service.Validations
         public UserValidation()
         {
             RuleFor(x => x.Name)
-                .NotEmpty().WithMessage("O campo Nome é obrigatório")
+                .NotEmpty().WithMessage(BaseValidation.RequiredField("Nome"))
                 .Length(min: 3, max: 20).WithMessage("O campo Nome deve ter entre 3 a 20 caracteres");
 
             RuleFor(x => x.Surname)
-                .NotEmpty().WithMessage("O campo Sobrenome é obrigatório")
+                .NotEmpty().WithMessage(BaseValidation.RequiredField("Sobrenome"))
                 .Length(min: 2, max: 20).WithMessage("O campo Nome deve ter entre 2 a 20 caracteres");
 
             RuleFor(x => x.BirthDate)
-                .NotEmpty().WithMessage("O Campo Data de Nascimento é obrigatório")
+                .NotEmpty().WithMessage(BaseValidation.RequiredField("Data de Nascimento"))
                 .Must(IsValidYear).WithMessage("O ano de nascimetno deve ser menor que o ano atual")
                 .Must(IsValidAge).WithMessage("O colaborador deve ter no mínimo 15 anos");            
         }
