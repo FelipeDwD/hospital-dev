a254215706f5427d46d40fcad9685c36acddd747
Felipe Arruda <felipeneves089@gmail.com>
2021-11-17 14:38:00 -0300

    GDA-14 Add reference to User Model

--- Hospital.WebApi.Domain/Models/ContactModel.cs
+++ Hospital.WebApi.Domain/Models/ContactModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Hospital.WebApi.Domain.Models
+﻿using System;
+
+namespace Hospital.WebApi.Domain.Models
 {
     public class ContactModel : BaseEntityModel
     {
         public string CellPhone { get; set; }
         public string Email { get; set; }
+        public Guid IdUser { get; set; }
+        public virtual UserModel User { get; set; }
     }
 }
--- Hospital.WebApi.Domain/Models/LoginModel.cs
+++ Hospital.WebApi.Domain/Models/LoginModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Hospital.WebApi.Domain.Models
+﻿using System;
+
+namespace Hospital.WebApi.Domain.Models
 {
     public class LoginModel : BaseEntityModel
     {
         public string Login { get; set; }
-        public string Password { get; set; }        
+        public string Password { get; set; }
+        public Guid IdUser { get; set; }
+        public virtual UserModel User { get; set; }
     }
 }
