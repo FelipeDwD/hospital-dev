eb340f5720dd27502956961174ab4b0f9a028fab
Felipe Arruda <felipeneves089@gmail.com>
2021-11-25 19:48:28 -0300

    GDA-22 Develope feature

--- Hospital.WebApi.Infra.Data/Repository/ContactRepository.cs
+++ Hospital.WebApi.Infra.Data/Repository/ContactRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Hospital.WebApi.Domain.Interfaces.Repository;
 using Hospital.WebApi.Domain.Models;
 using Hospital.WebApi.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Hospital.WebApi.Infra.Data.Repository
 {
@@ -11,5 +13,8 @@ namespace Hospital.WebApi.Infra.Data.Repository
         {
 
         }
+
+        public async Task<ContactModel> GetByEmailAsync(string email) =>
+            await _dbSet.FirstOrDefaultAsync(x => x.Email == email);       
     }
 }
--- Hospital.WebApi.Infra.Data/Repository/LoginRepository.cs
+++ Hospital.WebApi.Infra.Data/Repository/LoginRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Hospital.WebApi.Domain.Interfaces.Repository;
 using Hospital.WebApi.Domain.Models;
 using Hospital.WebApi.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace Hospital.WebApi.Infra.Data.Repository
 {
@@ -10,6 +12,6 @@ namespace Hospital.WebApi.Infra.Data.Repository
             : base(applicationDbContext)
         {
 
-        }
+        }      
     }
 }
